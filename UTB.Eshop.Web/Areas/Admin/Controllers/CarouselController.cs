619c625a71c23ddd37e3780bb9fe020da953aa1c
psc-utb <psc.utb@gmail.com>
2022-12-16 14:43:09 +0100

    Area Admin a CarouselController +Přidána Area Admin (Složka Areas + Admin) +Přidán CarouselController +Přidáno routování na Areu do Startup.cs +Přidány 2 metody Create      1. pro HttpGet -> zobrazení View      2. pro HttpPost -> zpracování formuláře +Přidáno View Create s formulářem +Přidán odkaz na formulář do navbaru (_Layout.cshtml)

---
+++ UTB.Eshop.Web/Areas/Admin/Controllers/CarouselController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UTB.Eshop.Domain.Entities;
+using UTB.Eshop.Infrastructure.Database;
+using UTB.Eshop.Web.Controllers;
+
+namespace UTB.Eshop.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CarouselController : Controller
+    {
+        readonly EshopDbContext _eshopDbContext;
+        public CarouselController(EshopDbContext eshopDbContext)
+        {
+            _eshopDbContext = eshopDbContext;
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(CarouselItem carouselItem)
+        {
+            _eshopDbContext.Add(carouselItem);
+            _eshopDbContext.SaveChanges();
+
+
+            return RedirectToAction(nameof(HomeController.Index), nameof(HomeController).Replace("Controller", String.Empty), new { area = String.Empty });
+            //return RedirectToAction("Index", "Home", new { area = "" });
+        }
+
+    }
+}
